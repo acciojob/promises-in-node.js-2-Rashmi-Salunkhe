34430e480c072e548ab0ad410bd8795871c7ea82
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-08-30 11:49:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,49 @@
 const fs = require('fs');
 
 function readFile(filePath) {
-  // TODO: Return a promise that resolves to the contents of the file
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(data);
+        });
+    });
 }
-
 function sumColumn(filePath, columnName) {
-  // TODO: Use readFile and promises to calculate the sum of the values in the specified column
+    readFile(filePath)
+        .then((data) => {
+            if (!data) {
+                throw new Error('CSV file is empty.');
+            }
+
+            const rows = data.split('\n');
+            const headers = rows[0].split(',');
+
+            const columnIndex = headers.indexOf(columnName);
+            if (columnIndex === -1) {
+                throw new Error(`Column '${columnName}' not found in the CSV.`);
+            }
+
+            let sum = 0;
+            for (let i = 1; i < rows.length; i++) {
+                const values = rows[i].split(',');
+                if (values.length > columnIndex) {
+                    sum += parseFloat(values[columnIndex]);
+                }
+            }
+
+            console.log(`The Sum of ${columnName} is ${sum}`);
+        })
+        .catch((err) => {
+            console.error(err.message);
+        });
+}
+const [,, filePath, columnName] = process.argv;
+
+if (!filePath || !columnName) {
+    console.error('Usage: node sumColumn.js <filePath> <columnName>');
+    process.exit(1);
 }
 
-// TODO: Call sumColumn with the command-line arguments and log the result to the console
+sumColumn(filePath, columnName);
